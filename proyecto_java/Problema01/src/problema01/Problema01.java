572b2401a4b5c8d6aea695c0a74a15a6f31e6f6b
Jorge Granda <jorjorgeandgranda06@gmail.com>
2025-01-23 12:55:24 -0500

    cambios

---
+++ proyecto_java/Problema01/src/problema01/Problema01.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package problema01;
+import
+import java.util.Scanner;
+
+/**
+ *
+ * @author Core i5-Pro
+ */
+public class Problema01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        int opcion;
+        String continuar;
+        int deportes[]= new int[7];
+        String cadena = "";
+        do{
+            System.out.println("Menu:\n 1.Futbol\n 2.Natacion\n 3.Atletismo\n"
+                    + "4.Basquetball\n 5.Ciclismo\n 6.Tenis\n 7.Yoga");
+        }
+        
+    }
+    
+}
